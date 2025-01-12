ecb0d5ab219767de2d49221233b9f07b9f73e649
nddmanh <manhnd.ilec@gmail.com>
2025-01-12 22:58:29 +0700

    API login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './configs/configuration';
 import { DatabaseModule } from './databases/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './ormconfig';
 import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
     TypeOrmModule.forRoot(ormConfig),
     DatabaseModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('jwtAuth').jwtTokenSecret,
+      }),
+    }),
     AuthModule,
   ],
   controllers: [AppController],
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,13 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dtos/register-user.dto';
+import { LoginDto } from './dtos/login.dto';
+import { AuthGuard } from './dtos/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(AuthGuard)
+  @Get()
+  test() {
+    return {
+      message: 'Oke',
+    };
+  }
+
   @Post('register')
   registerUser(@Body() body: RegisterUserDto) {
     return this.authService.registerUser(body);
+  }
+
+  @Post('login')
+  login(@Body() body: LoginDto) {
+    return this.authService.login(body);
   }
 }
